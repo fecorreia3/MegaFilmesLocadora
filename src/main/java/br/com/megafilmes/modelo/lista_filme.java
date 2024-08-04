ab2d2def60bbe7ce2940e51ea8d1fa65b3fbadde
fecorreia3 <fe.lima3@gmail.com>
2024-08-03 21:23:25 -0300

    primeiro

---
+++ src/main/java/br/com/megafilmes/modelo/lista_filme.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.br.com.megafilmes.modelo;
+
+import br.com.megafilmes.objeto.Filmes;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author fe_li
+ */
+public class lista_filme {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("LocadoraMegaFilmes");
+        EntityManager em = emf.createEntityManager();
+
+//        Filmes filme = new Filmes();
+        Filmes filme = em.find(Filmes.class, 2);
+        System.out.print(filme);
+
+        String jpql = "SELECT filmes FROM Filmes filmes";
+        Query query = em.createQuery(jpql);
+        List<Filmes> pessoas = em.getResultList();
+
+    }
+
+}
