801df82510068b9122ec7eb3bdf2cc6c89352701
fecorreia3 <fe.lima3@gmail.com>
2024-08-03 22:09:45 -0300

    criado tela de login e limpado as dependencias

--- src/main/java/br/com/megafilmes/modelo/lista_filme.java
+++ src/main/java/br/com/megafilmes/modelo/lista_filme.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package main.java.br.com.megafilmes.modelo;
+package br.com.megafilmes.modelo;
 
 import br.com.megafilmes.objeto.Filmes;
 import java.util.List;
